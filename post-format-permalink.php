f0ca63336ccf5cf888fa126b61b9a832b2454786
Yan Sarazin <yan@statikpulse.com>
2011-01-02 11:56:12 -0500

    Updated readme file. PostFormatPermalink class.

--- post-format-permalink.php
+++ post-format-permalink.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Post Format Permalink
 Plugin URI: http://statikpulse.com/post-format-permalink
-Description: 
-Version: 0.1
+Description: Post Format Permalink plugin gives you the ability to include the post format slug in your permalinks. Once the plugin is activated, simply include the %format% tag in your custom permalink.
+Version: 1.0
 Author: Yan Sarazin 
 Author URI: http://statikpulse.com
 */
@@ -25,72 +25,70 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-if (function_exists('add_action'))
-{
-	register_activation_hook(__FILE__, 'post_format_permalink_activate');
-	register_deactivation_hook(__FILE__, 'post_format_permalink_deactivate');
-
-	add_filter('pre_post_link', 'post_format_permalink', 10, 2);
-	add_filter('post_rewrite_rules', 'post_format_rewrite_rules');
-	add_filter('generate_rewrite_rules', 'post_format_generate_rewrite_rules');
+class PostFormatPermalink {
 	
-	global $clean_post_rewrites, $clean_rewrites;
-	$clean_post_rewrites = array();
-}
+	function __construct() {
+		register_activation_hook(__FILE__, array(&$this, 'activate'));
+		register_deactivation_hook(__FILE__, array(&$this, 'deactivate'));
 
-
-
-function post_format_permalink_activate()
-{
-	global $wp_rewrite;
-	$wp_rewrite->flush_rules();	
-}
-
-function post_format_permalink_deactivate()
-{
-	remove_filter('post_link', 'post_format_permalink');
-	remove_filter('post_rewrite_rules', 'post_format_rewrite_rules');
-	remove_filter('generate_rewrite_rules', 'post_format_generate_rewrite_rules');
+		add_filter('pre_post_link', array(&$this, 'generate_permalink'), 10, 2);
+		add_filter('post_rewrite_rules', array(&$this, 'rewrite_rules'));
+		add_filter('generate_rewrite_rules', array(&$this, 'generate_rewrite_rules'));
+		
+		global $clean_post_rewrites, $clean_rewrites;
+		$clean_post_rewrites = array();
+	}
 	
-	global $wp_rewrite;
-	$wp_rewrite->flush_rules();
+	function activate() {
+		global $wp_rewrite;
+		$wp_rewrite->flush_rules();
+	}
+	
+	function deactivate() {
+		remove_filter('post_link', array(&$this, 'generate_permalink'));
+		remove_filter('post_rewrite_rules', array(&$this, 'rewrite_rules'));
+		remove_filter('generate_rewrite_rules', array(&$this, 'generate_rewrite_rules'));
+
+		global $wp_rewrite;
+		$wp_rewrite->flush_rules();
+	}
+	
+	function generate_permalink($permalink, $post) {
+		if (strpos($permalink, '%format%') === FALSE) return $permalink;
+
+		if(!is_object($post)){
+			$post = get_post($post_id);
+		}
+
+		$format = get_post_format($post->ID);	
+		if (empty($format)){
+			$format = 'standard';
+		}
+
+		return str_replace('%format%', $format, $permalink);
+	}
+	
+	function generate_rewrite_rules($wp_rewrite) {
+		global $clean_post_rewrites;
+		$wp_rewrite->rules = $wp_rewrite->rules + $clean_post_rewrites;
+	}
+	
+	function rewrite_rules($post_rewrite) {
+		global $clean_post_rewrites;
+
+	  global $wp_rewrite;
+	  $wp_rewrite->use_verbose_page_rules = true;
+
+		while (list($k, $v) = each($post_rewrite)) {
+			$new_k = preg_replace('|%format%|', '('.implode('|', get_post_format_slugs()).')', $k, 1);
+			$clean_post_rewrites[$new_k] = $v;
+		}
+
+		return $post_rewrite;
+	}
+	
 }
 
-function post_format_permalink($permalink, $post) {
-	if (strpos($permalink, '%format%') === FALSE) return $permalink;
-
-	if(!is_object($post)){
-		$post = get_post($post_id);
-	}
-
-	$format = get_post_format($post->ID);	
-	if (empty($format)){
-		$format = 'standard';
-	}
-
-	return str_replace('%format%', $format, $permalink);
-} 
-
-
-function post_format_generate_rewrite_rules($wp_rewrite)
-{
-	global $clean_post_rewrites;
-	$wp_rewrite->rules = $wp_rewrite->rules + $clean_post_rewrites;
-}
-
-function post_format_rewrite_rules($post_rewrite)
-{
-	global $clean_post_rewrites;
-
-  global $wp_rewrite;
-  $wp_rewrite->use_verbose_page_rules = true;
-
-	while (list($k, $v) = each($post_rewrite)) {
-		$new_k = preg_replace('|%format%|', '('.implode('|', get_post_format_slugs()).')', $k, 1);
-		$clean_post_rewrites[$new_k] = $v;
-	}
-
-	return $post_rewrite;
-}
+$post_format = new PostFormatPermalink();
 
 ?>
--- readme.txt
+++ readme.txt
@@ -1,59 +1,25 @@
 === Posterize ===
 Contributors: Yan Sarazin
-Tags: post
+Tags: post, permalink, format, post format
 Requires at least: 3.1
 Tested up to: 3.1
-Stable tag: 0.1
+Stable tag: 1.0
 
-WordPress to Posterous plugin.
+Include post format slug in post permalink.
 
 == Description ==
 
-Posterize will cross-post your Wordpress blog post to any of your Posterous sites. Post methods include a link back to your WordPress blog, or complete content. Posterous
+Post Format Permalink plugin gives you the ability to include the post format slug in your permalinks. Once the plugin is activated, simply include the %format% tag in your custom permalink.
 
 
 == Installation ==
 
-1. If you are upgrading, it is recommended that you deactivate the plugin from the Plugins page, and delete the posterize folder from your server.
+1. If you are upgrading, it is recommended that you deactivate the plugin from the Plugins page, and delete the post-format-permalink folder from your server.
 2. Upload the unzipped contents to your /wp-content/plugins/ directory.
 3. Active the plugin from the Plugins page.
-4. Go to settings page to configure Posterize
-
-== Configuration == 
-
-1. Enter your Posterous email and password (used to log into http://posterous.com)
-2. Click "Refresh site list" and select the Posterous site you want to publish to
-3. Select Post Type
-4. Save Settings
 
 == Changelog ==
 
-= 2.2 =
-* Complete plugin rewrite.. again.
-* Updating a published WordPress post will now update the Posterous post
-* Pages no longer being published (Feature coming soon)
-* Improved admin panel layout and design
-* Selection of Posterous site changed to radio buttons
-
-= 2.1.1 =
-* Fixed missing line break issues.
-
-= 2.1.0 =
-* Including WordPress post tags if available
-* Passing Posterize as the source in the Posterous API call
-
-= 2.0.2 =
-* Fixed issues where an error was thrown when “get sites” would return a single Posterous site.
-
-= 2.0.1 =
-* Complete rewrite from the ground up
-* No longer uses email. Utilizes the Posterous API to interact with Posterous
-* Ability to select which Posterous site to post to
-* Administrator settings panel for easy configuration
-
-= 1.0.1 =
-* Changed Posterous email from post@posterous.com to posterous@posterous.com to avoid potential cross-post loop.
-
 = 1.0.0 =
-* Initial Posterize version.
+* Initial Release.
 
